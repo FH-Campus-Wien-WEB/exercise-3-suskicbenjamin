777aa96dbef29f9169dfc2a0264bd6bfaf3e026d
benjaminsuskic <suskicbenjamin@gmail.com>
2026-04-27 15:23:11 +0200

    finished exercise3

--- server/files/index.js
+++ server/files/index.js
@@ -20,25 +20,29 @@ function formatRuntime(runtime) {
 
 function appendMovie(movie, element) {
   new ElementBuilder("article").id(movie.imdbID)
-          .append(new ElementBuilder("img").with("src", movie.Poster))
-          .append(new ElementBuilder("h1").text(movie.Title))
-          .append(new ElementBuilder("p")
-              .append(new ElementBuilder("button").text("Edit")
-                    .listener("click", () => location.href = "edit.html?imdbID=" + movie.imdbID)))
-          .append(new ParagraphBuilder().items(
-              "Runtime " + formatRuntime(movie.Runtime),
-              "\u2022",
-              "Released on " +
-                new Date(movie.Released).toLocaleDateString("en-US")))
-          .append(new ParagraphBuilder().childClass("genre").items(movie.Genres))
-          .append(new ElementBuilder("p").text(movie.Plot))
-          .append(new ElementBuilder("h2").pluralizedText("Director", movie.Directors))
-          .append(new ListBuilder().items(movie.Directors))
-          .append(new ElementBuilder("h2").pluralizedText("Writer", movie.Writers))
-          .append(new ListBuilder().items(movie.Writers))
-          .append(new ElementBuilder("h2").pluralizedText("Actor", movie.Actors))
-          .append(new ListBuilder().items(movie.Actors))
-          .appendTo(element);
+    .append(new ElementBuilder("img").with("src", movie.poster))
+    .append(new ElementBuilder("h1").text(movie.title))
+    .append(new ElementBuilder("p")
+      .append(new ElementBuilder("button").text("Edit")
+        .listener("click", () => location.href = "edit.html?imdbID=" + movie.imdbID)))
+    .append(new ParagraphBuilder().items(
+      "Runtime " + formatRuntime(movie.runtime),
+      "\u2022",
+      "Released on " +
+      new Date(movie.released).toLocaleDateString("en-US")))
+    .append(
+      new ParagraphBuilder()
+      .childClass("genre")
+      .class("genres")
+      .items(movie.genres))
+    .append(new ElementBuilder("p").text(movie.plot))
+    .append(new ElementBuilder("h2").pluralizedText("Director", movie.directors))
+    .append(new ListBuilder().items(movie.directors))
+    .append(new ElementBuilder("h2").pluralizedText("Writer", movie.writers))
+    .append(new ListBuilder().items(movie.writers))
+    .append(new ElementBuilder("h2").pluralizedText("Actor", movie.actors))
+    .append(new ListBuilder().items(movie.actors))
+    .appendTo(element);
 }
 
 function loadMovies(genre) {
@@ -62,6 +66,9 @@ function loadMovies(genre) {
 
   const url = new URL("/movies", location.href)
   /* Task 1.4. Add query parameter to the url if a genre is given */
+  if (genre) {
+    url.searchParams.append("genre", genre);
+  }
 
   xhr.open("GET", url)
   xhr.send()
@@ -77,6 +84,26 @@ window.onload = function () {
          initialize them with a click handler that calls the 
          loadMovies(...) function above. */
       const genres = JSON.parse(xhr.responseText);
+
+      // "All" button first
+      const allButton = document.createElement("button");
+      allButton.textContent = "All";
+      allButton.addEventListener("click", function () {
+        loadMovies();
+      });
+      listElement.appendChild(allButton);
+
+      // Genre buttons
+      for (let i = 0; i < genres.length; i++) {
+        const button = document.createElement("button");
+        button.textContent = genres[i];
+
+        button.addEventListener("click", function () {
+          loadMovies(genres[i]);
+        });
+
+        listElement.appendChild(button);
+      }
 
       /* When a first button exists, we click it to load all movies. */
       const firstButton = document.querySelector("nav button");
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const movieModel = require('./movie-model.js');
 const app = express();
 
 // Parse urlencoded bodies
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, 'files')));
@@ -15,43 +15,72 @@ app.use(express.static(path.join(__dirname, 'files')));
    This endpoint returns a sorted array of all the genres of the movies
    that are currently in the movie model.
 */
+app.get("/genres", (req, res) => {
+  const genresSet = new Set();
+
+  const movies = Object.values(movieModel);
+
+  movies.forEach(movie => {
+    movie.genres.forEach(genre => {
+      genresSet.add(genre);
+    });
+  });
+
+  const genres = Array.from(genresSet).sort();
+
+  res.json(genres);
+});
 
 /* Task 1.4: Extend the GET /movies endpoint:
    When a query parameter for a specific genre is given, 
    return only movies that have the given genre
  */
 app.get('/movies', function (req, res) {
-  let movies = Object.values(movieModel)
-  res.send(movies);
-})
+  const genre = req.query.genre;
+
+  let movies = Object.values(movieModel);
+
+  if (genre) {
+    const filtered = [];
+
+    for (let i = 0; i < movies.length; i++) {
+      if (movies[i].genres.includes(genre)) {
+        filtered.push(movies[i]);
+      }
+    }
+
+    movies = filtered;
+  }
+
+  res.json(movies);
+});
 
 // Configure a 'get' endpoint for a specific movie
-app.get('/movies/:imdbID', function (req, res) {
-  const id = req.params.imdbID
-  const exists = id in movieModel
- 
-  if (exists) {
-    res.send(movieModel[id])
+app.get('/movies/:imdbID', (req, res) => {
+  const imdbID = req.params.imdbID;
+
+  const movie = movieModel[imdbID];
+
+  if (movie) {
+    res.json(movie);
   } else {
-    res.sendStatus(404)    
+    res.sendStatus(404);
   }
-})
+});
 
-app.put('/movies/:imdbID', function(req, res) {
 
-  const id = req.params.imdbID
-  const exists = id in movieModel
+app.put('/movies/:imdbID', function (req, res) {
+  const imdbID = req.params.imdbID;
+  const movie = req.body;
 
-  movieModel[req.params.imdbID] = req.body;
-  
-  if (!exists) {
-    res.status(201)
-    res.send(req.body)
+  if (movieModel[imdbID]) {
+    movieModel[imdbID] = movie;
+    res.sendStatus(200);
   } else {
-    res.sendStatus(200)
+    movieModel[imdbID] = movie;
+    res.status(201).json(movie);
   }
-  
-})
+});
 
 app.listen(3000)
 
